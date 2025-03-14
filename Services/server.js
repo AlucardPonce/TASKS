b6bfb771b882f492824f0c702cc64a734028c55c
Dev Jack <poncealucard@gmail.com>
2025-03-14 13:25:39 -0600

    update app

--- Services/server.js
+++ Services/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const speakeasy = require('speakeasy');
 
 app.use(cors());
 
@@ -12,18 +13,24 @@ app.use(express.json());
 const users = [];
 
 app.post('/register', async (req, res) => {
-
     const { email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     const secret = speakeasy.generateSecret({ length: 20 });
+
     const user = {
         email,
         password: hashedPassword,
         secret: secret.base32
     };
+
     users.push(user);
-    res.json({ secret: secret.base32 });
+
+    const otpauthUrl = `otpauth://totp/${email}?secret=${secret.base32}&issuer=MiApp`;
+
+    res.json({ secret: otpauthUrl }); // Enviamos la URL compatible con QR
+    console.log(users);
 });
+
 
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
@@ -33,28 +40,30 @@ app.post('/login', async (req, res) => {
         return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    if (!await bcrypt.compare(password, user.password)) {
-        return res.status(400).json({ message: 'Invalid credentials' });
-    }
-    res.json({ requiresMFA: true });
+    res.json({ requiresMFA: true, message: "Usuario autenticado. Ingresa el código OTP" });
 });
 
-app.post('verify-otp', async (req, res) => {
+
+app.post('/verify-otp', async (req, res) => {
     const { email, otp } = req.body;
     const user = users.find(user => user.email === email);
+
+    if (!user) {
+        return res.status(400).json({ message: "Usuario no encontrado" });
+    }
 
     const verify = speakeasy.totp.verify({
         secret: user.secret,
         encoding: 'base32',
-        token,
+        token: otp,
         window: 1
     });
 
     if (verify) {
-        res.json({ message: 'Success' });
+        res.json({ message: 'Inicio de sesión exitoso' });
     } else {
-        res.status(400).json({ success: false });
+        res.status(400).json({ message: 'Código OTP incorrecto' });
     }
 });
 
-app.listen(3001, () => {console.log('Server running on port 3001');});
+app.listen(3001, () => { console.log('Server running on port 3001'); });
